107df0184d9207789be3b34fe1431954d7f86d73
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2022-10-24 20:13:32 +0000

    methods_help practice

---
+++ main.py
@@ -0,0 +1,9 @@
+from methods_help import method_help
+# from methods_help import greetings
+# from functions import function
+# from returnStuff import returnS
+
+method_help()
+# greetings("Andrea")
+# function()
+# returnS()
