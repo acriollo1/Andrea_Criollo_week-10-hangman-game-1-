517413b7fef4c198f5599269a2cf048f4e77d6a0
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2022-11-07 20:37:31 +0000

    functions interactions

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from methods_help import method_help
+#from methods_help import method_help
 # from methods_help import greetings
 # from functions import function
 # from returnStuff import returnS
+from function_interactions import *
 
-method_help()
+sticks = ["-", "--", "---", "----"]
+my_mix = mix(sticks)
+
+try1 = try_your_luck()
+print(try1)
+
+verify_my_try(my_mix, try1)
+
+
+#method_help()
 # greetings("Andrea")
 # function()
 # returnS()
